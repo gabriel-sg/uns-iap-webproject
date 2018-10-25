ca2a172b6613692174de7671fd76c8505234dc1f
Gabriel SG <gsalazargisbert@gmail.com>
2018-10-25 18:23:11 -0300

    New RequestForm component

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RequestFormComponent } from './request-form/request-form.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent }
+  { path: 'requestform', component: RequestFormComponent }
 ];
 
 @NgModule({

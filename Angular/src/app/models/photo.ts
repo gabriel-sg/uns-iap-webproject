4a0fa8ffd6718b12dd65b44159d12c9081831a53
Seba Benamo <seba.benamo@gmail.com>
2018-11-28 11:33:33 -0300

    Photo model

---
+++ Angular/src/app/models/photo.ts
@@ -0,0 +1,14 @@
+export class Photo {
+    id?: number; //? porque es opcional. Viene del back-end
+    publi_id?: number;
+    filename: string;
+    created_at: string;
+    updated_at: string;
+    constructor() {
+        this.id = 0;
+        this.publi_id = 0;
+        this.filename = '';
+        this.created_at = '';
+        this.updated_at = '';
+    }
+}

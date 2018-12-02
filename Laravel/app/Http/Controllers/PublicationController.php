66e5065318992b3e8892e7a8d4173b5c85c25a63
Seba Benamo <seba.benamo@gmail.com>
2018-12-02 14:54:52 -0300

    Ahora si

--- Laravel/app/Http/Controllers/PublicationController.php
+++ Laravel/app/Http/Controllers/PublicationController.php
@@ -99,7 +99,7 @@ class PublicationController extends Controller
     {
         $photos = Photo::where('publi_id',$id)->get();
         foreach($photos as $photo){
-            $photoURLs[]='intuni.test/api/'.$photo->filename;
+            $photoURLs[]='intuni.test'.Storage::url($photo->filename);
         }
         echo json_encode($photoURLs);
     }
